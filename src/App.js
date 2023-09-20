4047413dd2f8910ea9e26825de201d383a813074
Tom Digby <tom.digby@gwtinsight.com>
2023-09-20 16:55:31 +0100

    Sass added

--- src/App.js
+++ src/App.js
@@ -1,43 +1,9 @@
+import Directory from './components/directory/directory.component';
 
 const App = () => {
 
-  const categories = [
-    {
-      id: 1,
-      title: 'Hats',
-    },
-    {
-      id: 2,
-      title: 'Jackets',
-    },
-    {
-      id: 3,
-      title: 'Trainers',
-    },
-    {
-      id: 4,
-      title: 'Womens',
-    },
-    {
-      id: 5,
-      title: 'Mens',
-    }
-  ];
-
   return (
-    <div className="categories-container">
-        {categories.map(({title}) => {
-            return (
-              <div className="category-container">
-                  <div className="background-image" />
-                  <div className="category-body-container">
-                      <h2>{title}</h2>
-                      <p>Shop Now</p>
-                  </div>
-              </div>
-            )
-        })}
-    </div>
+    <Directory />
   );
 }
 
