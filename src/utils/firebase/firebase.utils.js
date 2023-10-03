ab979eec078b08b66cdd3535a68a82ee6297dd8d
Tom Digby <tom.digby@gwtinsight.com>
2023-10-03 12:35:32 +0100

    Sign In Form

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -23,20 +23,38 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({ 
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({ 
     prompt: 'select_account' 
 });
 
 export const auth = getAuth();
-export const signInWithGoogle = () => signInWithPopup(auth, provider);
+export const signInWithGoogle = () => signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
 export const createUserDocumentFromAuth = async (userAuth) => {
+
     const userDocRef = doc(db, 'users', userAuth.uid);
-    console.log(userDocRef);
+
     const userSnapshot = await getDoc(userDocRef);
-    console.log(userSnapshot);
-    console.log(userSnapshot.exists());
+
+    if (!userSnapshot.exists()){
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try{
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        }
+        catch(error){
+            console.log('Error creating user', error.message);
+        }
+    }
+
+    return userDocRef;
+
 }
