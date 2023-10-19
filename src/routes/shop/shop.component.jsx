de1894fc7f5af855ba105a9fc82a317a103fc11a
Tom Digby <tom.digby@gwtinsight.com>
2023-10-19 16:57:10 +0100

    .

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,16 +1,17 @@
 import { useContext } from 'react';
 import { ProductContext } from '../../contexts/product.context';
-import SHOP_DATA from '../../shop-date.json'
+
+import ProductCard from '../../components/product-card/product-card.component';
+
+import './shop.styles.scss'
 
 const Shop = () => {
 
     const { products } = useContext(ProductContext);
 
-    return (<div>
-        {products.map(({id, name}) => (
-            <div key={id}>
-                <h1>{name}</h1>
-            </div>
+    return (<div className='products-container'>
+        {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
         ))}
     </div>)
 }
